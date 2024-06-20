7565fc0e6221a373c106e72c0519b7395aef1c28
Rethabile Diale <rethadiale27@gmail.com>
2024-06-20 22:32:04 +0200

    Implemented custom file input component with file selection feedback

---
+++ PodcastByRetha/src/components/CommonComponents/Input/FileInput.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function FileInput({ accept, id, fileHandleFnc, text }) {
+  const [fileSelected, setFileSelected] = useState("");
+
+  const onChange = (e) => {
+    console.log(e.target.files);
+    setFileSelected(e.target.files[0].name);
+    fileHandleFnc(e.target.files[0]);
+  };
+
+  return (
+    <>
+      <label
+        htmlFor={id}
+        className={`custom-input ${!fileSelected ? "label-input" : "active"}`}
+      >
+        {fileSelected ? `The File ${fileSelected} was Selected` : text}
+      </label>
+      <input
+        type="file"
+        accept={accept}
+        id={id}
+        style={{ display: "none" }}
+        onChange={onChange}
+      />
+    </>
+  );
+}
