d008a037c3768f9a2cfaa41f24393c6e8e34c41b
Rethabile Diale <rethadiale27@gmail.com>
2024-06-21 11:24:14 +0200

    Created Profile page to display user information and recently updated podcasts with logout functionality

--- PodcastByRetha/src/pages/Profile.jsx
+++ PodcastByRetha/src/pages/Profile.jsx
@@ -1,16 +1,38 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Header from "../components/CommonComponents/Header";
 import Button from "../components/CommonComponents/Button";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import { toast } from "react-toastify";
+import CarouselComponent from "../components/Carousel/CarouselComponent";
 
 export default function Profile() {
   const user = useSelector((state) => state.user.user);
-  console.log("My User", user);
-  if (!user) {
-    return <p>Loading...</p>;
-  }
+  const [podcasts, setPodcasts] = useState([]);
+
+  useEffect(() => {
+    const fetchPodcasts = async () => {
+      try {
+        const response = await fetch("https://podcast-api.netlify.app");
+        const data = await response.json();
+        const podcastsData = data.map((item) => ({
+          key: item.id,
+          id: item.id,
+          title: item.title,
+          displayImage: item.image,
+          // genre: genreMap[item.genres], // Map genre ID to title
+          // genreId: item.genre, // keep genre ID for filtering
+        }));
+        console.log(data);
+        setPodcasts(podcastsData);
+      } catch (error) {
+        console.error("Error fetching podcasts:", error);
+      }
+    };
+    console.log(fetchPodcasts());
+    fetchPodcasts();
+  }, []);
 
   const handleLogout = () => {
     signOut(auth)
@@ -24,6 +46,10 @@ export default function Profile() {
       });
   };
 
+  if (!user) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <Header />
@@ -32,6 +58,8 @@ export default function Profile() {
         <h1>{user.email}</h1>
         <h1>{user.uid}</h1>
         <Button text={"Logout"} onClick={handleLogout} />
+        <h2>Recently Updated Podcasts</h2>
+        <CarouselComponent items={podcasts} />
       </div>
     </div>
   );
