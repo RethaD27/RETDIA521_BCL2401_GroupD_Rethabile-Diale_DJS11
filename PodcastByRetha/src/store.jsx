eaaeeb0da9b2591a59ec374f4834291eadf02d5c
Rethabile Diale <rethadiale27@gmail.com>
2024-06-21 10:51:55 +0200

    Configured Redux store with reducers for user, podcast, and favorites

--- PodcastByRetha/src/store.jsx
+++ PodcastByRetha/src/store.jsx
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice";
 import podcastReducer from "./slices/podcastSlice";
+import favoritesReducer from "./slices/favouriteSlice";
 //  import episodeReducer from "./slices/slices/episodeSlice";
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -8,6 +9,7 @@ export default configureStore({
   reducer: {
     user: userReducer,
     podcast: podcastReducer,
+    favorites: favoritesReducer,
     // episode: episodeReducer,
   },
 });
