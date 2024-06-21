df6b0aa2422efd78db231236dd73ed7de3eb3d4a
Rethabile Diale <rethadiale27@gmail.com>
2024-06-21 11:29:48 +0200

    Created EpisodeDetails component with favorite functionality and audio playback

--- PodcastByRetha/src/components/Podcasts/EpisodeDetails.jsx
+++ PodcastByRetha/src/components/Podcasts/EpisodeDetails.jsx
@@ -1,5 +1,7 @@
+import { useDispatch, useSelector } from "react-redux";
 import Button from "../CommonComponents/Button";
 import PropTypes from "prop-types";
+import { addFavorite, removeFavorite } from "../../slices/favouriteSlice";
 
 export default function EpisodeDetails({
   index,
@@ -7,18 +9,37 @@ export default function EpisodeDetails({
   description,
   audioFile,
   onClick,
+  episodeId,
 }) {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites.episodes);
+
+  const isFavorite = favorites.some((episode) => episode.id === episodeId);
+
+  const handleFavoriteClick = () => {
+    if (isFavorite) {
+      dispatch(removeFavorite(episodeId));
+    } else {
+      dispatch(addFavorite({ id: episodeId, title, description, audioFile }));
+    }
+  };
+
   return (
     <div style={{ width: "100%" }}>
       <h1 style={{ textAlign: "left", marginBottom: 0 }}>
         {index}. {title}
       </h1>
-      <p style={{ marginLeft: "1.5rem" }} className="podcast-description ">
+      <p style={{ marginLeft: "1.5rem" }} className="podcast-description">
         {description}
       </p>
       <Button
         text={"Play"}
         onClick={() => onClick(audioFile)}
+        width={"100px"}
+      />
+      <Button
+        text={isFavorite ? "Unfavorite" : "Favorite"}
+        onClick={handleFavoriteClick}
         width={"100px"}
       />
     </div>
@@ -28,7 +49,7 @@ EpisodeDetails.propTypes = {
   index: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  audioFile: PropTypes.string.isRequired,
+  file: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
 };
 
